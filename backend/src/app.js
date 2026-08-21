0d406ed481f8581f2b19037253a3cc7af62db62e
Badal saxena <180774995+badalsaxena@users.noreply.github.com>
2026-08-21 12:34:06 +0530

    fix issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,6 +23,7 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
+    "https://code-guru-frontend-omega.vercel.app",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
